2968c51e143a576d52f9499d9d0c26c8762f1a03
christopherkiprop <choppachepa@gmail.com>
2024-07-13 12:35:46 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    // Your code to manipulate the DOM goes here
+});
+document.addEventListener("DOMContentLoaded", function() {
+    var paragraph = document.getElementById("text");
+    // Code to manipulate the paragraph element goes here
+});
+document.addEventListener("DOMContentLoaded", function() {
+    var paragraph = document.getElementById("text");
+    paragraph.textContent = "This is really cool!";
+});
